3f2955fc73204f086fde71f02eb03ad42f056a30
Veronica <91962861+phrog-exe@users.noreply.github.com>
2026-01-30 01:18:09 +0100

    funkcja żaba dnia, dwa nowe style buttonów, eksperymentowanie z ikonkami

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-console.log("Phrog.exe został pomyślnie załadowany! 🐸");
+const frogs = [
+    { name: "Żaba kapelusznik", file: "../assets/frogs/f1.jpg" },
+    { name: "pulpet", file: "../assets/frogs/f2.jpg" },
+    { name: "Żaba Moczarowa", file: "../assets/frogs/f3.jpg" }
+];
+
+function pickFrog() {
+    const randomIndex = Math.floor(Math.random() * frogs.length);
+    const frog = frogs[randomIndex];
+    
+    document.getElementById('random-frog').src = frog.file;
+    document.getElementById('frog-name').innerText = "Twój patron: " + frog.name;
+}
