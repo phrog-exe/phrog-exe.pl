0537c9c70ff08988a475a3c4bdd4538cc60c8d0f
Veronica <91962861+phrog-exe@users.noreply.github.com>
2026-01-31 21:22:05 +0100

    update sekcji projektów

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 const frogs = [
-    { name: "Żaba kapelusznik", file: "../assets/frogs/f1.jpg" },
-    { name: "pulpet", file: "../assets/frogs/f2.jpg" },
-    { name: "Żaba Moczarowa", file: "../assets/frogs/f3.jpg" }
+    { name: "Żaba kapelusznik!", file: "../assets/frogs/f1.jpg" },
+    { name: "Wallace's flying frog!", file: "../assets/frogs/f2.jpg" },
+    { name: "Żaba Moczarowa", file: "../assets/frogs/f3.jpg" },
+    { name: "Pumpkin toadlet - tiny frog!", file: "../assets/frogs/f4.jpg" },
+    { name: "Painted reed frog!", file: "../assets/frogs/f5.jpg" }
 ];
 
 function pickFrog() {
